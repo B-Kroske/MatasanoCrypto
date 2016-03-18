327f300063734647a0466b0456d3f132d86b97ae
B-Kroske <b.kroske@gmail.com>
2016-03-17 20:51:05 -0400

    Added xor function based on Challenge 2 in Set 1.

--- CryptoLib.py
+++ CryptoLib.py
@@ -14,10 +14,21 @@ def validate(guess, ans, isVerbose = False):
 
 
 
-#Takes a hex string or binary array and converts it to base 64
+#Takes a hex string or a byte array/string to convert to base 64
 def toBase64(val):
-    print(type(val).__name__)
     if(type(val).__name__ == 'str'):
         return base64.b64encode(bytes.fromhex(val))
-    elif(type(val).__name__ == 'bytes'):
+    elif(type(val).__name__ == 'bytearray' or
+        type(val).__name__ == 'bytes'):
         return base64.b64encode(val)
+
+#Xors two byte arrays
+#Resulting array is the same length as the first array.
+def xor(arr1, arr2):
+    len1 = len(arr1)
+    len2 = len(arr2)
+    res = bytearray()
+
+    for i in range(0,len1):
+        res.append(arr1[i] ^ arr2[i % len2])
+    print(res)
