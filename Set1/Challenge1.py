f26714e66e284e1e45e57db0721879a71b5e0f4a
B-Kroske <b.kroske@gmail.com>
2016-03-17 20:48:30 -0400

    Modified to use bytearray instead of bytes

--- Set1/Challenge1.py
+++ Set1/Challenge1.py
@@ -4,7 +4,7 @@ import CryptoLib
 def main():
     ans = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
     hexStr = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
-    byteArr = bytes.fromhex(hexStr)
+    byteArr = bytearray.fromhex(hexStr)
 
     encodedStr = base64.b64encode(byteArr)
 
